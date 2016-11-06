6790bdad58dd5ab153528fa4743e79a836c96c1f
PT <PT@PT-PC>
2016-11-06 21:38:19 +0100

    - County & Feedback implemented

---
+++ model/src/main/java/model/County.java
@@ -0,0 +1,16 @@
+package model;
+
+public class County {
+	
+	private String name;
+	
+	public County() {
+	}
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
