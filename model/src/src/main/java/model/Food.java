860a7213217db3b0de01a3c505bd7906e7102818
blaketillman159 <bukovenszkitamas92@gmail.com>
2016-11-07 20:05:43 +0100

    Food model létrehozva

---
+++ model/src/src/main/java/model/Food.java
@@ -0,0 +1,65 @@
+package src.main.java.model;
+
+import java.util.List;
+
+public class Food {
+	
+	public enum foodTypes {
+		Main,
+		Soup,
+		Breakfast,
+		Dessert,		
+		Pizza,
+		Fastfood,
+		Dish,
+		Garnish,
+		Salads,
+		Vegetarian,
+		Drinks,	
+	}
+	
+	private int id;
+	private String name;
+	private foodTypes foodTypes;
+	private int price;
+	private String description;
+	private List<String> ingredients;	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public foodTypes getFoodTypes() {
+		return foodTypes;
+	}
+	public void setFoodTypes(foodTypes foodTypes) {
+		this.foodTypes = foodTypes;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public List<String> getIngredients() {
+		return ingredients;
+	}
+	public void setIngredients(List<String> ingredients) {
+		this.ingredients = ingredients;
+	}
+
+}
