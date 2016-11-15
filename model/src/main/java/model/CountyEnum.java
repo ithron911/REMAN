19ce16a92add80957962324cc7d87beca233b456
x <x@x-VirtualBox>
2016-11-15 16:44:04 +0100

    - CountyEnum "update"

--- model/src/main/java/model/CountyEnum.java
+++ model/src/main/java/model/CountyEnum.java
@@ -1,23 +1,33 @@
 package model;
 
 public enum CountyEnum {
-	BÁCS_KISKUN,
-	BARANYA,
-	BÉKÉS,
-	BORSOD_ABAÚJ_ZEMPLÉN,
-	CSONGRÁD,
-	FEJÉR,
-	GYŐR_MOSON_SOPRON,
-	HAJDÚ_BIHAR,
-	HEVES,
-	JÁSZ_NAGYKUN_SZOLNOK,
-	KOMÁROM_ESZTERGOM,
-	NÓGRÁD,
-	PEST,
-	SOMOGY,
-	SZABOLCS_SZATMÁR_BEREG,
-	TOLNA,
-	VAS,
-	VESZPRÉM,
-	ZALA;
+	BÁCS_KISKUN("Bács-Kiskun"),
+	BARANYA("Baranya"),
+	BÉKÉS("Békés"),
+	BORSOD_ABAÚJ_ZEMPLÉN("Borsod-Abaúj-Zemplén"),
+	CSONGRÁD("Csongrád"),
+	FEJÉR("Fejér"),
+	GYŐR_MOSON_SOPRON("Győr-MOson-Sopron"),
+	HAJDÚ_BIHAR("Hajdú-Bihar"),
+	HEVES("Heves"),
+	JÁSZ_NAGYKUN_SZOLNOK("Jász-Nagykun-Szolnok"),
+	KOMÁROM_ESZTERGOM("Komárom-Esztergom"),
+	NÓGRÁD("Nógrád"),
+	PEST("Pest"),
+	SOMOGY("Somogy"),
+	SZABOLCS_SZATMÁR_BEREG("Szaboly-Szatmár-Bereg"),
+	TOLNA("Tolna"),
+	VAS("Vas"),
+	VESZPRÉM("Veszprém"),
+	ZALA("Zala");
+	
+	private String countyName;
+	
+	private CountyEnum(String countyName) {
+		this.countyName = countyName;
+	}
+	
+	public String getCountyName() {
+		return countyName;
+	}
 }
