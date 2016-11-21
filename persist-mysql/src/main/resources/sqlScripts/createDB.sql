c66fdd2cc4c5d107e627287a2905bace6bd285f2
Pankucsi Mate <pankucsi.mate@simplesoft.hu>
2016-11-21 21:22:30 +0100

    - database creation sql added

--- persist-mysql/src/main/resources/sqlScripts/createDB.sql
+++ persist-mysql/src/main/resources/sqlScripts/createDB.sql
@@ -1,6 +1,56 @@
 CREATE DATABASE REMAN;
 USE REMAN;
 
-CREATE USER "RemanDeveloper"@"localhost" IDENTIFIED BY "RemanPass";
+CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin';
+GRANT ALL PRIVILEGES ON REMAN.* TO 'admin'@'localhost';
+FLUSH PRIVILEGES;
 
-GRANT *.* TO "RemanDeveloper"@"localhost";
+
+CREATE TABLE Restaurant(
+	Name VARCHAR(30) NOT NULL,
+	Founding VARCHAR(30),
+	Type VARCHAR(30),
+	PRIMARY KEY ( Name )
+)
+
+CREATE TABLE Food(
+	ID INT NOT NULL AUTO_INCREMENT,
+    Name VARCHAR(100) NOT NULL,
+	RestaurantName VARCHAR(30),
+    FoodType VARCHAR(50),
+    Price INT,
+    PRIMARY KEY ( ID ),
+	FOREIGN KEY ( RestaurantName ) REFERENCES Restaurant(Name)
+);
+
+CREATE TABLE ATable(
+	ID INT NOT NULL AUTO_INCREMENT,
+	TableType VARCHAR(50),
+	RestaurantName VARCHAR(30),
+	isReserved BOOLEAN,
+	PRIMARY KEY ( ID ),
+	FOREIGN KEY ( RestaurantName ) REFERENCES Restaurant(Name)
+);                  
+
+CREATE TABLE Payment(
+	ID INT NOT NULL AUTO_INCREMENT,
+	PaymentType VARCHAR(30),
+	RestaurantName VARCHAR(30),
+	PRIMARY KEY ( ID ),
+	FOREIGN KEY ( RestaurantName ) REFERENCES Restaurant(Name)
+);
+
+
+CREATE TABLE Reservation(
+	Price INT,
+    DateBook DATE,
+    Comment Varchar(200),
+	RestaurantName VARCHAR(30),
+	PaymantID INT,
+FoodID INT,
+		FOREIGN KEY ( RestaurantName ) REFERENCES Restaurant(Name),
+	FOREIGN KEY ( FoodID) REFERENCES Food(ID),
+	FOREIGN KEY ( PaymantID ) REFERENCES Payment(ID)
+                         );					
+
+FOREIGN KEY ( ) REFERENCES table( );
