fe8281a941eb63309deb6638792a781e0037fdc0
x <x@x-VirtualBox>
2016-11-29 17:05:33 +0100

    - config changes

---
+++ persist-mysql/src/main/resources/sqlScripts/deleteRecords.sql
@@ -0,0 +1,5 @@
+delete from Reservation;
+delete from Payment;
+delete from ATable;
+delete from Food;
+delete from Restaurant;
--- persist-mysql/src/test/java/sqlscripttests/SQLScriptLoadTests.java
+++ persist-mysql/src/test/java/sqlscripttests/SQLScriptLoadTests.java
@@ -2,39 +2,104 @@ package sqlscripttests;
 
 import java.io.IOException;
 import java.io.Reader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.jdbc.ScriptRunner;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assume;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import com.mysql.cj.jdbc.Driver;
+
+
 
 public class SQLScriptLoadTests {
 
+	private static String HOST;
+	private static Integer PORT;
+	private static String DATABASE;
+	private static String USER;
+	private static String PASSWORD;
+
+	@BeforeClass
+	public static void beforeClass() {
+
+		try {
+			HOST = System.getProperty("database.host");
+			PORT = Integer.parseInt(System.getProperty("database.port"));
+			DATABASE = System.getProperty("database.db");
+			USER = System.getProperty("database.user");
+			PASSWORD = System.getProperty("database.password");
+		} catch (NumberFormatException ex) {
+			Assume.assumeNoException(ex);
+		}
+
+		runSQLScript("sqlScripts/createDB.sql");
+
+	}
+
+	@AfterClass
+	public static void afterClass() {
+
+		runSQLScript("sqlScripts/dropDB.sql");
+
+	}
+
+	private static void runSQLScript(String scriptFilePath) {
+
+		try {
+			Class.forName(Driver.class.getName());
+		} catch (ClassNotFoundException e) {
+			Assume.assumeNoException(e);
+		}
+		final String connectionURL = String.format("jdbc:mysql://%s:%d/%s", HOST, PORT, DATABASE);
+		Connection connection;
+		try {
+			connection = DriverManager.getConnection(connectionURL, USER, PASSWORD);
+			connection.setAutoCommit(false);
+			ScriptRunner runner = new ScriptRunner(connection);
+
+			Reader createDBReader = Resources.getResourceAsReader(scriptFilePath);
+			runner.runScript(createDBReader);
+			createDBReader.close();
+		} catch (SQLException e) {
+			Assume.assumeNoException(e);
+		} catch (IOException e) {
+			Assume.assumeNoException(e);
+		}
+		
+	}
+
+	@Before
+	public void setUp() {
+		
+			runSQLScript("sqlScripts/insertDB.sql");
+		
+	}
+
+	@After
+	public void tearDown() {
+		
+			runSQLScript("sqlScripts/deleteRecords.sql");
+		
+	}
+
 	@Test
 	public void testSQLScripts() {
-		try {
-			Reader confReader = Resources.getResourceAsReader("configuration/testConfig.xml");
-			SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(confReader);
-			SqlSession sqlSession = sqlSessionFactory.openSession();
-			ScriptRunner runner = new ScriptRunner(sqlSession.getConnection());
-
-			Reader createDBReader = Resources.getResourceAsReader("sqlScripts/createDB.sql");
-			Reader fillUpDBReader = Resources.getResourceAsReader("sqlScripts/insertDB.sql");
-			Reader deleteDBReader = Resources.getResourceAsReader("sqlScripts/dropDB.sql");
-			
-			runner.runScript(createDBReader);
-			runner.runScript(fillUpDBReader);
-			runner.runScript(deleteDBReader);
-
-			confReader.close();
-			createDBReader.close();
-			fillUpDBReader.close();
-			deleteDBReader.close();
-			sqlSession.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		System.out.println("Test 001");
+	}
+	
+	@Test
+	public void testSQLScripts1() {
+		System.out.println("Test 02");
 	}
 }
